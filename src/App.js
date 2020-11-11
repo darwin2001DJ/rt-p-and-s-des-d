148d89243e165f30ced126eb175200e92de6bd7f
Your Name <darwinmuthu450@gmail.com>
2020-11-11 15:20:51 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import CollegeDetails from "./CollegeDetails";
+import "./App.css";
+class App extends Component {
+  constructor() {}
+  render = () => {
+    const { collegeData } = this.state;
+    return (
+      <div>
+        <h1 className="header">Ebox Colleges</h1>
+      </div>
+    );
+  };
 }
-
 export default App;
