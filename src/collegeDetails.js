8bc5813fed9b149685963f47632d01634bfa15c7
Your Name <darwinmuthu450@gmail.com>
2020-11-13 07:11:55 +0530

    data added

--- src/collegeDetails.js
+++ src/collegeDetails.js
@@ -1,50 +1,7 @@
-let collegeData = [
-  {
-    id: 1,
-    name: "Kathir college of engineering",
-    location: "Coimbatore",
-    affilatedTo: "Anna University",
-    TNEAcode: 2018,
-    averagePlacementRecord: 88.5,
-    coursesOffered: [
-      "Information Technology",
-      "Computer Science ",
-      "Electrical and Electronics",
-    ],
-  },
-  {
-    id: 2,
-    name: "Shanmuga college of engineering",
-    location: "Coimbatore",
-    affilatedTo: "EBox University",
-    TNEAcode: 2028,
-    averagePlacementRecord: 76,
-    coursesOffered: [
-      "Electrical and Electronics",
-      "Electronics and Communication ",
-      "Computer Science",
-    ],
-  },
-  {
-    id: 3,
-    name: "United Institute of Technology",
-    location: "Coimbatore",
-    affilatedTo: "Anna University",
-    TNEAcode: 2056,
-    averagePlacementRecord: 100,
-    coursesOffered: [
-      "Information Technology",
-      "Agriculture ",
-      "Marine",
-      "Computer Science",
-    ],
-  },
-  {
-    id: 4,
-    name: "VSB college of engineering",
-    location: "Karur",
-    affilatedTo: "Anna University",
-    TNEAcode: 2078,
-    averagePlacementRecord: 90,
-  },
-];
+import React, { Component } from "react";
+
+export default class collegeDetails extends Component {
+  render() {
+    return <></>;
+  }
+}
